6422f278b4b96b798df310497b3392cb35a21b52
Kaushal Sharma <Ksharma17052005@gmail.com>
2026-02-05 14:58:00 +0530

    Adding server.js

---
+++ server.js
@@ -0,0 +1,13 @@
+// Write a JS program to create a HTTP server using Node.js
+
+import http from "http";
+
+const server = http.createServer((req, res) => {
+    res.statusCode = 200;
+    res.setHeader("Content-Type", "text/plain");
+    res.end("Hello World\n");
+});
+
+server.listen(3000, () => {
+    console.log("Server running at http://localhost:3000/\n");
+});
